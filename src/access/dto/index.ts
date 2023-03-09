36ef80f506bc16c612c91ee375ddeed4d94d467b
zemetia <sclous2012@gmail.com>
2023-03-09 23:19:12 +0700

    fix: forgot to save | delete user module

--- src/access/dto/index.ts
+++ src/access/dto/index.ts
@@ -2,3 +2,6 @@ export * from './create-role.dto'
 export * from './update-role.dto'
 export * from './delete-role.dto'
 
+export * from './create-permission.dto'
+export * from './update-permission.dto'
+export * from './delete-permission.dto'
