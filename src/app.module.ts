1bfa3065972e47e42e8e2e8a8621d05015766891
zemetia <sclous2012@gmail.com>
2023-03-09 23:20:35 +0700

    fix: remove UserModule from app Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { AccessModule } from './access/access.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule, AccessModule],
+  imports: [PrismaModule, AuthModule, AccessModule],
   providers: [AuthService],
 })
 export class AppModule {}
